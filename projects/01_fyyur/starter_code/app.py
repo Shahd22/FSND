30c01153efa3dcc739013e076cc31d897309051d
Shahd Alsayari <Shahdalsayari@gmail.com>
2020-09-10 20:42:15 +0300

    Add delete Venue

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -293,7 +293,7 @@ def create_venue_submission():
         #print(sys.exc_info())
     finally:
         db.session.close()
-        return render_template('pages/home.html') 
+        return render_template('pages/home.html')
 
 
   # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
@@ -302,12 +302,25 @@ def create_venue_submission():
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
 def delete_venue(venue_id):
-  # TODO: Complete this endpoint for taking a venue_id, and using
+  # TODO: [Done] Complete this endpoint for taking a venue_id, and using
   # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
+  try:
+      venue_name = venue.name
+      Venue.query.filter_by(id=venue_id).delete()
+      db.session.commit()
+      flash(venue_name + 'Venue was successfully deleted')
+
+  except:
+      db.session.rollback()
+      flask('An error occurred. ' + venue_name + 'could not be deleted')
+  finally:
+      db.session.close()
+
+
 
   # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
   # clicking that button delete it from the db then redirect the user to the homepage
-  return None
+  #return None
 
 #  Artists
 #  ----------------------------------------------------------------
